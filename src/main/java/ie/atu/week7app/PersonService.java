215d3b1cee3840d0b5f0e7a9adf97c8a3b68ec5f
G00410156@atu.ie <g00410156@atu.ie>
2023-11-15 14:40:25 +0000

    First COmmit

---
+++ src/main/java/ie/atu/week7app/PersonService.java
@@ -0,0 +1,4 @@
+package ie.atu.week7app;
+
+public class PersonService {
+}
---
+++ src/main/java/ie/atu/week7app/Week7AppApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week7app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week7AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week7AppApplication.class, args);
+    }
+
+}
