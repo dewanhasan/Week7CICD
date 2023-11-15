188e75b1c3124865fab30de82f1cb768be1b78b3
G00410156@atu.ie <g00410156@atu.ie>
2023-11-15 15:23:43 +0000

    Second Commit

--- src/main/java/ie/atu/week7app/Person.java
+++ src/main/java/ie/atu/week7app/Person.java
@@ -1,4 +1,38 @@
 package ie.atu.week7app;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class Person {
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min =2, max =50, message = "Name Must be between 2 and 50 characters.")
+    private String name;
+
+    @NotBlank(message = "Title cannot be blank")
+    @Size(min =2, max =5, message = "Name Must be between 2 and 5 characters.")
+    private String title;
+
+    @NotBlank(message = "EmployeeId cannot be blank")
+    private String employeeId;
+
+    @Min(value = 0, message = "Age must be greater than or equal to 0")
+    private int age;
+
+
+    @Email(message = "Invalid Email address")
+    @Size(min =2, max =50, message = "Email Must be between 2 and 50 characters.")
+    private String email;
+
+    @NotBlank(message = "Position cannot be blank")
+    private String position;
+
+    @NotBlank(message = "Department cannot be blank")
+    private String department;
+
 }
--- src/main/java/ie/atu/week7app/PersonService.java
+++ src/main/java/ie/atu/week7app/PersonService.java
@@ -1,4 +1,16 @@
 package ie.atu.week7app;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PersonService {
+
+    public void savePerson(Person person){
+        System.out.println("Person saved: " + person);
+    }
+
+    public Person getPersonByEmployeeId(String employeeId){
+        return new Person();
+    }
+
 }
